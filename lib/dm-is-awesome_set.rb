5cb12439bc1d857be525d6a865e429e6d7bd51b4
snusnu <gamsnjaga@gmail.com>
2009-05-20 04:34:40 +0200

    removed usage of rubygems

--- lib/dm-is-awesome_set.rb
+++ lib/dm-is-awesome_set.rb
@@ -1,6 +1,5 @@
 require 'pathname'
 ['dm-core', 'dm-adjust', 'dm-aggregates', 'dm-validations'].each do |dm_var|
-  gem dm_var, '>=0.9.7'
   require dm_var
 end
 require Pathname(__FILE__).dirname.expand_path / 'dm-is-awesome_set' / 'is' / 'awesome_set.rb'
