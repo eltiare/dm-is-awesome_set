c04b243fcf3fa7ba54d9c7b2c1fa7b6ffa49d46a
felix <felix@seconddrawer.com.au>
2010-03-29 18:01:30 +0800

    transactions as class methods

--- lib/dm-is-awesome_set.rb
+++ lib/dm-is-awesome_set.rb
@@ -157,7 +157,7 @@ module DataMapper
         # @see move_without_saving
 
         def move(vector)
-          transaction do
+          get_class.transaction do
             move_without_saving(vector)
             save!
           end
@@ -249,7 +249,7 @@ module DataMapper
           # Trigger all the before :destroy methods
           sads.each { |sad| before_methods.each { |bf| sad.send(bf) } }
           # dup is called here because destroy! likes to clear out the array, understandably.
-          transaction do
+          get_class.transaction do
             sads.dup.destroy!
             adjust_gap!(full_set, lft, -(rgt - lft + 1))
           end
@@ -260,7 +260,7 @@ module DataMapper
         # Same as @destroy, but does not run the hooks
         def destroy!
           sad = self_and_descendants
-          transaction do
+          get_class.transaction do
             sad.dup.destroy!
             adjust_gap!(full_set, lft, -(rgt - lft + 1))
           end
