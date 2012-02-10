11284ff6d78b7a5b4eb1dd2256fef26037500431
snusnu <gamsnjaga@gmail.com>
2012-02-10 13:15:28 +0100

    Make the parent FK field optional
    DM changed the default behavior of `belongs_to` to
    be required by default.

--- lib/dm-is-awesome_set.rb
+++ lib/dm-is-awesome_set.rb
@@ -39,7 +39,8 @@ module DataMapper
         property :rgt, Integer, :writer => :private, :index => true
 
         class_opts = {:model => self.name, :child_key => opts[:child_key], :order => [:lft.asc] }
-        belongs_to :parent,  class_opts
+
+        belongs_to :parent,   class_opts.merge!(:required => false)
         has n,     :children, class_opts
 
         before :save do
